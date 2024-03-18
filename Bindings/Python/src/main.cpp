e40e95a476057f7e1ff695e57e3449bea36e1c6c
eoudejans <eoudejans@objectvision.nl>
2024-03-18 12:40:09 +0100

    Initial working python bindings test setup.

--- Bindings/Python/src/main.cpp
+++ Bindings/Python/src/main.cpp
@@ -1,10 +1,13 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 
-int add(int i, int j) {
+int add(int i, int j) 
+{
     return i + j;
 }
 
-PYBIND11_MODULE(PythonBindings, m) {
+PYBIND11_MODULE(PythonBindings, m) 
+{
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
     m.def("add", &add, "A function that adds two numbers");
